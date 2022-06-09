1d43f813c18f155023597dace549af05688fd0b9
Linh Vu <linh777gkog@gmail.com>
2022-06-09 22:16:54 +0700

    new Image

---
+++ main.js
@@ -0,0 +1,9 @@
+// Begin
+var canvas = document.getElementById("gamezone");
+var context = document.getContext("2d");
+var scoreshow = document.getElementById("score");
+
+var birdimg = new Image();
+var hinhnenchinh = new Image();
+var ongtren = new Image();
+var ongduoi = new Image();
