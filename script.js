30a6afda58827a54cb9f81f991e46d66662b6aa5
Linh Vu <linh777gkog@gmail.com>
2022-06-09 23:04:03 +0700

    push code

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("keypress", handleStart, { once: true });
+const title = document.querySelector("[data-title]");
+
+function handleStart() {
+  title.classList.add("hide");
+}
+
+function handleLose() {}
